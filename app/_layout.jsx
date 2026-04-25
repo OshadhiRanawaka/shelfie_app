eeec71bd630d3e905f49c80486dc5b67a921f97a
Oshadhi <oshadhiranawaka@gmail.com>
2026-04-25 19:44:15 +0530

    Refactor RootLayout to include Stack navigation and remove footer

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,10 +4,17 @@ import { Stack } from 'expo-router'
 
 const RootLayout = () => {
   return (
-    <View style={{ flex: 1 }}>
-        <Stack/>
-        <Text>Footer</Text>
-    </View>
+
+    <Stack screenOptions={{
+      headerStyle: { backgroundColor: '#ddd' },
+      headerTintColor: '#333'
+    }}>
+      <Stack.Screen name="index" options={{ title: 'Home'}} />
+      <Stack.Screen name="about" options={{ title: 'About' }} />
+      <Stack.Screen name="contact" options={{ title: 'Contact' , headerShown: false}} />
+    </Stack>
+
+
   )
 }
 
