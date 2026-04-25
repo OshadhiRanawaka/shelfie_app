2026aaf23c6e136bbcd09d4fd00d87a13e4e6caa
Oshadhi <oshadhiranawaka@gmail.com>
2026-04-25 20:37:24 +0530

    Implement themed components: add ThemedView, ThemedText, ThemedLogo, ThemedCard; refactor About, Contact, and Home components for theme support

--- app/contact.jsx
+++ app/contact.jsx
@@ -1,33 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Link, LinkMenu } from 'expo-router'
+import { Link } from 'expo-router'
+import { StyleSheet } from 'react-native'
+
+import ThemedView from '../components/ThemedView'
+import ThemedText from '../components/ThemedText'
 
 const Contact = () => {
   return (
-    <View style={styles.container}>
-          <Text style={styles.title}>Contact Page</Text>
+    <ThemedView style={styles.container}>
+      <ThemedText style={styles.title} title={true}>Contact Page</ThemedText>
 
-          <Link href="/" style={styles.link}>
-            Go Back Home
-          </Link>
-    </View>
+      <Link href="/" style={styles.link}>
+        <ThemedText>Home Page</ThemedText>
+      </Link>
+    </ThemedView>
   )
 }
 
 export default Contact
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 18
-    },
-    link: {
-        marginVertical: 10,
-        borderBottomWidth: 1,
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1
+  }
 })
--- app/index.jsx
+++ app/index.jsx
@@ -1,44 +1,51 @@
-import { StyleSheet, Text, View , Image} from 'react-native'
-import React from 'react'
-import  {Link} from 'expo-router'
-import Logo from '../assets/img/logo_light.png'
+import { StyleSheet } from 'react-native'
+import { Link } from 'expo-router'
+
+import ThemedView from "../components/ThemedView"
+import ThemedText from "../components/ThemedText"
+import ThemedLogo from "../components/ThemedLogo"
+import Spacer from "../components/Spacer"
 
 const Home = () => {
   return (
-      <View style={styles.container}>
-          <Image source={Logo} style={styles.image} />
-          <Text style={styles.title}>The Number 1</Text>
+    <ThemedView style={styles.container}>
+      <ThemedLogo />
+      <Spacer />
 
-          <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+      <ThemedText style={styles.title} title={true}>The Number 1</ThemedText>
 
-          <Link href ="/about" style={styles.link}>
-            About Page
-          </Link>
-          <Link href ="/contact" style={styles.link}>
-            Contact Page
-          </Link>
-    </View>
+      <ThemedText style={{ marginTop: 10, marginBottom: 30 }}>
+        Reading List App
+      </ThemedText>
+
+      <Link href="/about" style={styles.link}>
+        <ThemedText>About Page</ThemedText>
+      </Link>
+
+      <Link href="/contact" style={styles.link}>
+        <ThemedText>Contact Page</ThemedText>
+      </Link>
+    </ThemedView>
   )
 }
 
 export default Home
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 18
-    },
-    image: {
-        marginVertical: 20,
-    },
-    link: {
-        marginVertical: 10,
-        borderBottomWidth: 1,
-    }
-
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img: {
+    marginVertical: 20
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1
+  }
 })
