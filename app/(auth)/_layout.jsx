d3ba081aa79565729116c7e7dcb32c6905d4e74b
Oshadhi <oshadhiranawaka@gmail.com>
2026-05-01 17:29:45 +0530

    chore: update dependencies and add devDependencies for TypeScript and React types
    - Added \react-dom\ version 19.1.0 to package.json and package-lock.json
    - Added devDependencies for TypeScript (version 5.9.2) and React types (version 19.1.10) in package.json
    - Updated package-lock.json to reflect changes in dependencies and removed unused Radix UI components

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,13 +1,11 @@
-import { Stack } from "expo-router"
-import { StatusBar } from "react-native"
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function AuthLayout() {
   return (
     <>
       <StatusBar style="auto" />
-      <Stack
-        screenOptions={{ headerShown: false, animation: "none" }}
-      />
+      <Stack screenOptions={{ headerShown: false, animation: "none" }} />
     </>
-  )
+  );
 }
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,29 +1,27 @@
-import { StyleSheet, Text, View, useColorScheme} from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
-import {Colors} from "../constants/Colors"
-import { StatusBar } from 'expo-status-bar'
+import { Stack } from "expo-router";
+import { Colors } from "../constants/Colors";
+import { useColorScheme } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-const RootLayout = () => {
-  const colorScheme = useColorScheme()
-  const theme = Colors[colorScheme] ?? Colors.light
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
 
   return (
     <>
-    <StatusBar value ="auto"/>
-    <Stack screenOptions={{
-      headerStyle: { backgroundColor: theme.navBackground },
-      headerTintColor: theme.text
-      }}>
-        <Stack.Screen name="(auth)" options={{headerShown: false}}/>
-        <Stack.Screen name="index" options={{ title: 'Home' }} />
-    </Stack>
-      </>
-
-
-  )
+      <StatusBar style="auto" />
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTintColor: theme.title,
+        }}
+      >
+        {/* Individual Screens */}
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        {/* Groups */}
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
 }
-
-export default RootLayout
-
-const styles = StyleSheet.create({})
