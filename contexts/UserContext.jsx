d3c8445f8f52a87c55e866780771665c8ccd3f1e
Oshadhi <oshadhiranawaka@gmail.com>
2026-05-06 11:37:19 +0530

    Update Profile component to display user email and add logout functionality; enhance UserContext for initial user state management

--- contexts/UserContext.jsx
+++ contexts/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { account } from "../lib/appwrite";
 import { ID } from "react-native-appwrite";
 
@@ -6,6 +6,7 @@ export const UserContext = createContext();
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
+  const[authChecked, setAuthChecked] = useState(false);
 
   async function login(email, password) {
     try {
@@ -25,10 +26,28 @@ export function UserProvider({ children }) {
     }
   }
 
-  async function logout() {}
+  async function logout() {
+    await account.deleteSession();
+    setUser(null);
+  }
+
+  async function getInitialUserValue() {
+    try {
+      const response = await account.get();
+      setUser(response);
+
+    } catch (error) {
+      setUser(null);
+    } finally {
+      setAuthChecked(true);
+    }
+  }
+  useEffect(() => {
+    getInitialUserValue();
+  }, []);
 
   return (
-    <UserContext.Provider value={{ user, login, register, logout }}>
+    <UserContext.Provider value={{ user, login, register, logout , authChecked}}>
       {children}
     </UserContext.Provider>
   );
